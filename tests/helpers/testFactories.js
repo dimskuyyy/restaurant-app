57a0be2cf9d05e38fdc79a4c108d8443e87a9f1f
dimskuyyy <dimasfauzan1712@gmail.com>
2023-11-30 20:22:19 +0700

    transisi ke testing

---
+++ tests/helpers/testFactories.js
@@ -0,0 +1,2 @@
+import DbFavoriteRestaurant from "../../src/scripts/data/favorite-idb";
+// import 
---
+++ tests/likeRestaurant.test.js
@@ -0,0 +1,5 @@
+describe("Like a Restaurant", () => {
+  it("Should be able to display the like button when the restaurant has not been liked yet", async() => {
+    
+  });
+});
